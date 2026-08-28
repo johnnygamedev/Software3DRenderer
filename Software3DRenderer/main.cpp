feb14f98d2e71b129b5fc6bd74af2804a84f377c
johnny macmillan <jpmac67@icloud.com>
2026-08-28 14:57:17 +0100

    adding the window
    adding the fixed custom window

--- Software3DRenderer/main.cpp
+++ Software3DRenderer/main.cpp
@@ -3,11 +3,32 @@
 #include <iostream>
 #include <cstdint>
 
-
+#undef main
+ 
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;  
 
 int main(int argc, char* argv[]) {
     bool isRunning = true;
     SDL_Event event;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Software 3D Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	if (!window) {
+		std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
+	
+	
 
     while (isRunning) {
         while (SDL_PollEvent(&event)) {
