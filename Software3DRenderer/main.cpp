2d515cf0f8f0ab0393be4783468f27f16b43cee9
johnny macmillan <jpmac67@icloud.com>
2026-08-28 15:03:30 +0100

    im chainging the wrong thuinf

--- Software3DRenderer/main.cpp
+++ Software3DRenderer/main.cpp
@@ -13,14 +13,14 @@ int main(int argc, char* argv[]) {
     SDL_Event event;
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+		std::cerr << "BOY BOY it didnt work!  " << SDL_GetError() << std::endl;
 		return 1;
 	}
 
-	SDL_Window* window = SDL_CreateWindow("Software 3D Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	SDL_Window* window = SDL_CreateWindow("Silly little renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
 	if (!window) {
-		std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+		std::cerr << "son son WINDOW DIDNT WORK! " << SDL_GetError() << std::endl;
 		SDL_Quit();
 		return 1;
 	}
@@ -28,7 +28,7 @@ int main(int argc, char* argv[]) {
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
 	
-	std::vector<uint32_t> pixelBuffer(SCREEN_WIDTH * SCREEN_HEIGHT, 0xFF301934);
+	std::vector<uint32_t> pixelBuffer(SCREEN_WIDTH * SCREEN_HEIGHT, 0xFF000000);
 
     while (isRunning) {
         while (SDL_PollEvent(&event)) {
@@ -36,7 +36,7 @@ int main(int argc, char* argv[]) {
                 isRunning = false;
             }
         }
-		std::fill(pixelBuffer.begin(), pixelBuffer.end(), 0xFF301934);
+		std::fill(pixelBuffer.begin(), pixelBuffer.end(), 0xFF000000);
     }
 	return 0;
 }
